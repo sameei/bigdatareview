70966753631843facd38d7ad9cd2de0c848f9128
Reza Same'ei <reza.samee@gmail.com>
2020-10-28 18:25:02 +0330

    Added Deps & Elaborate Tutorials Order

--- javawork/src/main/java/javawork/Main.java
+++ javawork/src/main/java/javawork/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World :)");
+
+        /*
+        Tutorials in Order:
+        - Kafka
+        - ParseLog
+        - Json
+        - Avro
+        - KafkaStream
+        */
     }
 
 }
