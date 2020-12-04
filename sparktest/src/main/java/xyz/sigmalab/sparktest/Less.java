58d423385287f819eb4693db9b6c434e83c1c258
Reza Same'ei <reza.samee@gmail.com>
2020-12-04 19:34:36 +0330

    3.0-SNAPSHOT

--- sparktest/src/main/java/xyz/sigmalab/sparktest/Less.java
+++ sparktest/src/main/java/xyz/sigmalab/sparktest/Less.java
@@ -6,12 +6,23 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class Less {
 
+    // $SPARK_HOME/bin/spark-submit \
+    //      --class xyz.sigmalab.sparktest.Less \
+    //      --master spark://192.168.1.114:7077 \
+    //      ./target/sparktest-1.0-SNAPSHOT.jar \
+    //      hdfs:///path-to-file/file.txt hdfs:///path-for-result
+
     public static void main(String[] args) {
+
+        String src = args[0];
+        System.out.println("SRC: " + src);
+        String dest = args[1];
+        System.out.println("DESC: "+ dest);
 
         SparkConf conf = new SparkConf().setAppName("Less");
         JavaSparkContext jctx = new JavaSparkContext(conf);
-        JavaRDD<String> jlines = jctx.textFile("file:///tmp/viminfo");
-        jlines.saveAsTextFile("/tmp/viminfo-newone");
+        JavaRDD<String> jlines = jctx.textFile(src);
+        jlines.saveAsTextFile(dest);
 
     }
 
